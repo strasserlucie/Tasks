655557e1b717f439ff99826cf1e9f75557c60e10
strasserlucie <ls00007@mix.wvu.edu>
2021-02-08 11:06:22 -0500

    strasserlucie Task 02 upload c

--- task02.R
+++ task02.R
@@ -1,4 +1,4 @@
-setwd('~/Desktop/Evolution/tasks/Task_02')
+setwd('~/Desktop/Evolution/Tasks/Task_02')
 Data <- read.csv ('http://jonsmitchell.com/data/beren.csv', stringsAsFactors=F)
 write.csv(Data, 'rawdata.csv', quote=F)
 length(Data)
@@ -11,6 +11,7 @@ Data[2,]
 Data[1:3,]
 Data[1:3, 4]
 Data[1:5, 1:3]
+
 Feeds <- which(Data[,9] == 'bottle')
 berenMilk <- Data[Feeds ,]
 head(berenMilk)
@@ -18,6 +19,7 @@ Feeds <- which(Data[,'event'] == 'bottle')
 head(Feeds)
 Feeds <- which(Data$event == 'bottle')
 head(Feeds)
+
 dayID <- apply(Data, 1, function(x) paste(x[1:3],collapse='-'))
 head(dayID)
 dateID <- sapply(dayID, as.Date, format = "%Y-%m-%d", origin = "2019-04-18")
@@ -27,19 +29,30 @@ head(data)
 beren2<-Data
 beren3<- beren2[order(beren2$age) ,]
 write.csv(beren3, 'beren_new.csv', quote=F, row.names=FALSE)
+
+# start of part b
+setwd('~/Desktop/Evolution/Tasks/Task_02')
+beren3 <- read.csv('beren_new.csv', stringsAsFactors=F)
+
 Feeds <- which(beren3$event == "bottle")
 avgMilk <- mean(beren3$value[Feeds])
 avgFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], mean)
 varFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], var)
 totalFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], sum)
-numFeeds <- tapply(beren3$value[Feeds] ,  beren3$age[Feeds], length)
+numFeeds <- tapply(beren3$value[Feeds], beren3$age[Feeds], length)
+
 head(numFeeds)
+
 cor(beren3$value[Feeds], beren3$age[Feeds])
 cor.test(beren3$value[Feeds], beren3$age[Feeds])
+
 berenCor <- cor.test(beren3$value[Feeds], beren3$age[Feeds])
+
 head(berenCor)
+
 berenANOVA <- aov(beren3$value[Feeds] ~ beren3$caregiver[Feeds])
 head(berenANOVA)
+
 boxplot( beren3$value[Feeds] ~ beren3$caregiver[Feeds], xlab= "who gave the bottle" , ylab = "amount of milk consumed (oz)" )
 par(las=1, mar=c(5,5,1,1), mgp=c(2, 0.5, 0), tck=-0.01)
 plot(as.numeric(names(totalFeed)), totalFeed, type="b", pch=16, xlab="age in days", ylab="ounces of milk")
@@ -49,3 +62,21 @@ plot(as.numeric(names(totalFeed)) , totalFeed , type="b" , pch=16, xlab=" age in
 abline(h=mean(totalFeed) , lty=2, col='red')
 dev.off()
 source("http://jonsmitchell.com/code/plotFxn02b.R")
+
+Hypothesis tested for part)c) There is a relationship between the type of diaper change (  bowel movement) and amount eaten. 
+
+Extra credit:
+beren4<- beren3[Naps,]
+
+#Start of c:
+
+numNaps <- tapply(beren3$value[Naps] , beren3$age[Naps], length)
+cor( beren3$value[Naps], beren3$age[Naps])
+ cor.test(beren3$value[Naps], beren3$age[Naps])
+ berenCor<- cor.test(beren3$value[Naps], beren3$age[Naps])
+ summary(berenCor)
+ berenANOVA<- aov(beren3$value[Naps] ~ beren3$age[Naps])
+ boxplot( beren3$value[Naps] ~ beren3$age[Naps], xlab= "how old was he", ylab = "amount slept (min)")
+ par(las=1, mar=c(5,5,1,1), mgp=c(2, 0.5, 0), tck=-0.01)
+ plot(as.numeric(names(totalNaps)), totalNaps , type="b" , pch=16, xlab="age in days", ylab="amount slept")
+ 
